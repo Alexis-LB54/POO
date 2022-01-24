db4e19dd8d6b870ffb68d8c8f3dd4d5a35f05d2f
Alexis-LB54 <alexis@lebail.eu>
2022-01-24 10:06:40 +0100

    poo correction

--- src/vues/modifyEmployee.php
+++ src/vues/modifyEmployee.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 session_start();
 ?>
 <!DOCTYPE html>
@@ -10,7 +10,6 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PAGE MODIFIER D'UN EMPLOYÉ</title>
     <style>
-
         #form_controller {
             display: flex;
             flex-direction: column;
@@ -24,46 +23,31 @@ session_start();
 
 <body>
     <div>
-        <?php 
-            if (array_key_exists("error", $_SESSION)) {
-               echo $_SESSION["error"];
-               unset($_SESSION["error"]);
-            }
+        <?php
+        if (array_key_exists("error", $_SESSION)) {
+            echo $_SESSION["error"];
+            unset($_SESSION["error"]);
+        }
         ?>
     </div>
-    <?php 
-        $id = isset($_SESSION["employeeDatas"]["id"]) ? $_SESSION["employeeDatas"]["id"] : "";
+    <?php
+    $id = isset($_SESSION["employeeDatas"]["id"]) ? $_SESSION["employeeDatas"]["id"] : "";
     ?>
-    <form action=<?="/employee/$id"?> method="POST" id="form_controller">
+    <form action=<?= "/employee/$id" ?> method="POST" id="form_controller">
         <label for="lastname">NOM: </label>
-        <input 
-            type="text" 
-            name="lastname" 
-            id="lastname"
-            value="<?php
-               echo isset($_SESSION["employeeDatas"]["lastname"]) 
-               ? $_SESSION["employeeDatas"]["lastname"] 
-               : "";
-            ?>"
-       />
+        <input type="text" name="lastname" id="lastname" value="<?php
+                                                                echo isset($_SESSION["employeeDatas"]["lastname"])
+                                                                    ? $_SESSION["employeeDatas"]["lastname"]
+                                                                    : "";
+                                                                ?>" />
         <label for="firstname">PRENOM: </label>
-        <input 
-            type="text" 
-            name="firstname" 
-            id="firstname"
-            value="<?php
-               echo isset($_SESSION["employeeDatas"]["firstname"]) ? $_SESSION["employeeDatas"]["firstname"] : "";
-            ?>"
-        />
+        <input type="text" name="firstname" id="firstname" value="<?php
+                                                                    echo isset($_SESSION["employeeDatas"]["firstname"]) ? $_SESSION["employeeDatas"]["firstname"] : "";
+                                                                    ?>" />
         <label for="badge_number">NUMERO DE BADGE: </label>
-        <input 
-            type="number" 
-            name="badge_number" 
-            id="badge_number"
-            value="<?php
-               echo isset($_SESSION["employeeDatas"]["badge_number"]) ? $_SESSION["employeeDatas"]["badge_number"] : "";
-            ?>"
-        />
+        <input type="number" name="badge_number" id="badge_number" value="<?php
+                                                                            echo isset($_SESSION["employeeDatas"]["badge_number"]) ? $_SESSION["employeeDatas"]["badge_number"] : "";
+                                                                            ?>" />
         <input type="submit" value="ENREGISTRER LES MODIFICATIONS">
     </form>
 </body>
